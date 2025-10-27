864d09ebf9d4870e80ca803e8d16aeb103120430
Lalesh Kumar Raj <laleshkumarraj2005@gmail.com>
2025-10-27 19:09:31 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,6 +15,17 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"))
 app.use(cookieParser());
 
+// Routes would be added here
+import userRouter from './routes/user.routes.js'
+
+// routes decelaration
+// app.get pahle likh ke hi  kaam chal jaata thaa but ab nahi chlega because iles seperate hai toh ab middlewre ka use karna hoga
+
+app.use("/api/v1/users",userRouter)
+
+// url banega like this -- http://localhost:8000/api/v1/users/register
+
+
 
 
 export default app;
